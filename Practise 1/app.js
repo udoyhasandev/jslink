e2f32a457f9566662d45a5c526a01c8eaeb43245
MD UDOY HASAN ORKO <dev@udoyhasan.com>
2021-07-03 13:53:58 +0600

    Initial Commit

---
+++ Practise 1/app.js
@@ -0,0 +1,28 @@
+// This a a single line comment
+
+// Topic Usages of console
+
+console.log("Hello World");
+console.log(123);
+console.log(true);
+
+var greetings = "Hello";
+console.log(greetings);
+console.log([1,2,3,4]);
+console.log({a:1, b:2});
+console.table({a:1, b:2});
+
+console.error("This is some error");
+console.warn("This is a warning")
+console.time("Hello")
+  console.log("Hello world")
+  console.log("Hello world")
+  console.log("Hello world")
+  console.log("Hello world")
+  console.log("Hello world")
+console.timeEnd("Hello")
+/*
+  This is an 
+  multi line
+  comment
+*/
